8d6d0a10bd85f5871fcb1d685642f324c7a63d4c
faizadli <mochfaizadli@gmail.com>
2021-02-23 13:18:28 +0700

    Hari Ketujuh

--- Todo App/View/ContentView.swift
+++ Todo App/View/ContentView.swift
@@ -16,6 +16,8 @@ struct ContentView: View {
   @State private var showingAddTodoView: Bool = false
   @State private var showingSettingsView: Bool = false
   
+  @EnvironmentObject var iconSettings: IconNames
+  
   var body: some View {
     NavigationView{
       ZStack{
@@ -42,7 +44,7 @@ struct ContentView: View {
                 .imageScale(.large)
             }
           .sheet(isPresented: $showingSettingsView){
-            SettingsView()
+            SettingsView().environmentObject(self.iconSettings)
           })
         
         if todos.count == 0{
